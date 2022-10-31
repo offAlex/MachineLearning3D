cab1d8ccd3ddca0cbf6752230b48943f9988d7ed
offAlex <off.sokal.al@gmail.com>
2022-10-31 12:46:30 +0300

    Import MLAgent

---
+++ Assets/Scripts/MoveToCoinsAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class MoveToCoinsAgent : Agent
+{
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+}
