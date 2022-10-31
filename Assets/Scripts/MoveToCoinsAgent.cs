e47da7d0ffb62673779e5740560bd5e38547bc7e
offAlex <off.sokal.al@gmail.com>
2022-10-31 13:50:25 +0300

    Написан основной скрипт

--- Assets/Scripts/MoveToCoinsAgent.cs
+++ Assets/Scripts/MoveToCoinsAgent.cs
@@ -1,14 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using Random = System.Random;
 
 public class MoveToCoinsAgent : Agent
 {
+    [SerializeField] private Transform targetTransform;
 
-    public override void OnActionReceived(ActionBuffers actions)
+    public override void OnEpisodeBegin()
     {
-        
+        transform.position = new Vector3(0, 1, 0);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+
+    public override void OnActionReceived(ActionBuffers action)
+    {
+        float moveX = action.ContinuousActions[0];
+        float moveZ = action.ContinuousActions[1];
+        float moveSpeed = 3f;
+
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousAction = actionsOut.ContinuousActions;
+        continuousAction[0] = Input.GetAxisRaw("Horizontal");
+        continuousAction[1] = Input.GetAxisRaw("Vertical");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<Coin>(out Coin coin)){
+            SetReward(+1f);
+            EndEpisode();
+        }
+        if(other.TryGetComponent<Wall>(out Wall wall)){
+            SetReward(-1f);
+            EndEpisode();
+        }
     }
 }
+
